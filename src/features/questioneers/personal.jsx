212497fb5acaed78baf54981367c0caaaba719bf
Clanbulance <107610779+Clanbulance@users.noreply.github.com>
2025-09-01 23:37:21 +0200

    changed datastructure

--- src/features/questioneers/personal.jsx
+++ src/features/questioneers/personal.jsx
@@ -2,23 +2,24 @@ import React from "react";
 
 function    Personal(
     {formData,
-    setFormData
+    setFormData,
+    ChangeFormData
     }
 ){
 
     return ( 
         <div className="questineerForm">
             <label htmlFor="NameInput">Name:</label>
-            <input type="text" id="NameInput" name="NameInput" value={formData.name} onChange={() => setFormData()}/>
+            <input type="text" id="NameInput" name="NameInput" value={formData.name} onChange={() => ChangeFormData()}/>
 
             <label htmlFor="FamilyInput">Family Name:</label>
-            <input type="text" id="FamilyInput" name="FamilyInput" value={formData.familyName}/> 
+            <input type="text" id="FamilyInput" name="FamilyInput" value={formData.familyName} onChange={() => ChangeFormData()}/> 
 
             <label htmlFor="DateOfBirth">Date of Birth:</label>
-            <input type="date" id="DateOfBirth" name="DateOfBirth" value={formData.dateOfBirth}/>
+            <input type="date" id="DateOfBirth" name="DateOfBirth" value={formData.dateOfBirth} onChange={() => ChangeFormData()}/>
 
             <label htmlFor="PhoneNumber">Phone Number:</label>
-            <input type="number" id="PhoneNumber" name="PhoneNumber" value={formData.phoneNumber}/>
+            <input type="number" id="PhoneNumber" name="PhoneNumber" value={formData.phoneNumber} onChange={() => ChangeFormData()}/>
 
             <button type="submit">Submit</button>
             
